5753709df8653bd92b9cdb5b99d0dc972f5954a1
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-07-23 16:59:14 -0700

    we refactored our auth.test.js in order to use our new data-helper, tests are passing BITCHES

--- test/auth.test.js
+++ test/auth.test.js
@@ -1,26 +1,8 @@
-require('dotenv').config();
+const { getAgent, getUsers } = require('./data-helper');
 
-const request = require('supertest');
-const app = require('../lib/app');
-const connect = require('../lib/utils/connect');
-const mongoose = require('mongoose');
-const User = require('../lib/models/User');
-
-describe('app routes', () => {
-  beforeAll(() => {
-    connect();
-  });
-
-  beforeEach(() => {
-    return mongoose.connection.dropDatabase();
-  });
-
-  afterAll(() => {
-    return mongoose.connection.close();
-  });
-
+describe('auth routes', () => {
   it('can create a new user', () => {
-    return request(app)
+    return getAgent()
       .post('/api/v1/auth/signup')
       .send({
         username: 'lalall',
@@ -36,50 +18,31 @@ describe('app routes', () => {
       });
   });
 
-  it('can sign in an existing user and provide token', async() => {
-    const user = await User.create({
-      username: 'lalall',
-      password: 'wkejrnwkejrn',
-      profilePhotoUrl: 'hebrjwhebwjebr'
-    });
-
-    return request(app)
+  it('can sign in an existing user and provide token', () => {
+    const user = getUsers()[0];
+    return getAgent()
       .post('/api/v1/auth/signin')
       .send({
-        username: 'lalall',
-        password: 'wkejrnwkejrn',
+        username: user.username,
+        password: 'password',
       })
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
-          username: 'lalall',
-          profilePhotoUrl: 'hebrjwhebwjebr'
+          username: user.username,
+          profilePhotoUrl: expect.any(String)
         });
       });
   });
   
-  it('can verify a user with a token', async() => {
-    const user = await User.create({
-      username: 'lalall',
-      password: 'wkejrnwkejrn',
-      profilePhotoUrl: 'hebrjwhebwjebr'
-    });
-    const agent = request.agent(app);
-    return agent 
-      .post('/api/v1/auth/signin')
-      .send({
-        username: 'lalall',
-        password: 'wkejrnwkejrn',
-      })
-      .then(() => {
-        return agent
-          .get('/api/v1/auth/verify');
-      })
+  it('can verify a user with a token', () => {
+    return getAgent() 
+      .get('/api/v1/auth/verify')
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
-          username: 'lalall',
-          profilePhotoUrl: 'hebrjwhebwjebr'
+          username: expect.any(String),
+          profilePhotoUrl: expect.any(String)
         });
       });
   });
